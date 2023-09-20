71c20cda8ed0d1eb8e91580def7f1b7118880520
Samuel Amojo <57438689+Psamcyite@users.noreply.github.com>
2023-09-19 21:54:40 -0500

    changed something
    Co-Authored-By: daniela <100431+daniela@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,2 +1,11 @@
 // hey this is a comment
-console.log("Hello World")
+console.log("Hello World");
+
+function main() {
+    return null;
+    console.log(1);
+}
+main()
+
+// console.log(main)
+main();
